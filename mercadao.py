d1b11d1f169882b355bfe1698b002772e5c7b164
t0118430 <antonio.rocha.pm@gmail.com>
2024-07-18 22:26:25 +0100

    fix txt file

--- mercadao.py
+++ mercadao.py
@@ -62,7 +62,7 @@ if data['count'] != 0:
     cenas = data
     orders_to_keep = []
     orders = []
-    with open('results.log', 'r') as file:
+    with open('order_track.txt', 'r') as file:
         lines = file.readlines()
         lines = [line.strip() for line in orders]
     for item in data['orders']:      
