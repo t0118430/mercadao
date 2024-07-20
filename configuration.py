7e3df27a6c412c777700d175d5d82c11536848d5
t0118430 <antonio.rocha.pm@gmail.com>
2024-07-20 21:53:39 +0100

    fixed configs

--- configuration.py
+++ configuration.py
@@ -7,16 +7,9 @@ class Configuration:
         formatted_date = midnight_utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')
 
         #envs
-        #self.url = os.getenv('URL')
-        #self.url_raw = os.getenv('URL_AVAILABLE')
-        #self.url_available = self.url_raw + formatted_date
-        #self.topic = os.getenv('TOPIC')
-        #self.username = os.getenv('USERNAME')
-        #self.password = os.getenv('PASSWORD')
-
-        self.url = "http://localhost:5000/token"
-        url_raw = "localhost:5000"
-        self.url_available = "http://localhost:5000/orders"
-        self.topic = "trabalhaboidevizUmS9N1lPsQJpN"
-        self.username = "username"
-        self.password = "password"
+        self.url = os.getenv('URL')
+        self.url_raw = os.getenv('URL_AVAILABLE')
+        self.url_available = self.url_raw + formatted_date
+        self.topic = os.getenv('TOPIC')
+        self.username = os.getenv('USERNAME')
+        self.password = os.getenv('PASSWORD')
